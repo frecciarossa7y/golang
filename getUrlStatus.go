551c06f47e03fc5e46d30276598d3da7a2294f0c
Masaaki YOSHIZAWA <frecciarossa7y@users.noreply.github.com>
2015-03-16 01:08:23 +0900

    Create getUrlStatus.go

---
+++ getUrlStatus.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+var empty struct{}
+
+func getStatus(urls []string) <-chan string {
+    statusChan := make(chan string, 3)
+    limit := make(chan struct{}, 5)
+    go func() {
+        for _, url := range urls {
+            select {
+            case limit <- empty:
+                go func(url string) {
+                    res, err := http.Get(url)
+                    if err != nil {
+                        log.Fatal(err)
+                    }
+                    statusChan <- res.Status
+                    <-limit
+                }(url)
+            }
+        }
+    }()
+    return statusChan
+}
+
+func main() {
+    urls := []string{
+        "http://www.google.com",
+        "http://gunosy.com/",
+        "http://www.yahoo.co.jp/",
+        "http://golang.org/",
+        "http://golang.jp/",
+    }
+    statusChan := getStatus(urls)
+    
+    for i := 0; i < len(urls); i++ {
+        fmt.Println(<-statusChan)
+    }
+}
